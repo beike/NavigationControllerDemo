698689f81ccdb97abf68b5fd409a5700542f74ea
becky <becky@beckydeMacBook-Pro.local>
2015-08-16 09:16:01 +0800

    Initial Commit

---
+++ NavigationControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NavigationControllerDemo
+//
+//  Created by becky on 15/8/16.
+//  Copyright (c) 2015年 becky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NavigationControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NavigationControllerDemo
+//
+//  Created by becky on 15/8/16.
+//  Copyright (c) 2015年 becky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
